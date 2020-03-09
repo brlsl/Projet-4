148dd064e56a2358762ebb480014808c4cb34880
Bruno LAN SUN LUK <lansunluk@gmail.com>
2020-03-09 21:52:43 +0100

    instrumented test order added

--- app/src/androidTest/java/com/example/mareu/MeetingInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mareu/MeetingInstrumentedTest.java
@@ -1,8 +1,10 @@
-package com.example.mareu.instrumented_tests;
+package com.example.mareu;
 
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
+import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
@@ -14,9 +16,11 @@ import com.example.mareu.MyMeetingAdapter;
 import com.example.mareu.R;
 import com.example.mareu.di.DI;
 import com.example.mareu.model.Meeting;
+import com.example.mareu.service.DummyMeetingApiService;
 import com.example.mareu.service.DummyMeetingApiServiceGenerator;
 import com.example.mareu.service.MeetingApiService;
 import com.example.mareu.utils.DeleteMeetingAction;
+import com.example.mareu.utils.RecyclerViewMatcher;
 
 import org.hamcrest.Matchers;
 import org.junit.Rule;
@@ -25,15 +29,23 @@ import org.junit.runner.RunWith;
 
 import java.util.List;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.AllOf.allOf;
 import static com.example.mareu.utils.RecyclerViewItemCountAssertion.withItemCount;
 
@@ -118,6 +130,24 @@ public class MeetingInstrumentedTest {
     }
 
     @Test
+    public void orderingByPlaceShouldWorks(){
+        onView(withId(R.id.item_menu))
+                .perform(click());
+        onView(withText("Du plus ancien au plus récent"))
+                .perform(click());
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(0, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion C - 19:50 - Luigi")));
+        onView(withId(R.id.item_menu))
+                .perform(click());
+        onView(withText("Du plus récent au plus ancien"))
+                .perform(click());
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(2, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion C - 19:50 - Luigi")));
+    }
+
+    @Test
     public void shouldRemoveMeeting() {
         onView(withId(R.id.recyclerViewList))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, new DeleteMeetingAction()));
@@ -145,6 +175,24 @@ public class MeetingInstrumentedTest {
         onView(allOf(withId(R.id.recyclerViewList), isDisplayed())).check(withItemCount(ITEM_COUNT));
     }
 
+    @Test
+    public void orderingByDateShouldWorks(){
+        onView(withId(R.id.item_menu))
+                .perform(click());
+        onView(withText("Du plus ancien au plus récent"))
+                .perform(click());
+        onView(withId(R.id.recyclerViewList))
+                .perform(RecyclerViewActions.scrollToPosition(0));
+        onView(withId(R.id.recyclerViewList))
+                .check(matches(withText("")));
+        /*
+       onView(withId(R.id.item_menu))
+                .perform(click());
+        onView(withText("Du plus récent au plus ancien"))
+                .perform(click());
+        */
+
+    }
 
 
 }
