886772c9647b69d5ee935dbc43e4b211ec1a377d
Bruno LAN SUN LUK <lansunluk@gmail.com>
2020-03-09 23:01:59 +0100

    all instrumented tests done

--- app/src/androidTest/java/com/example/mareu/MeetingInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mareu/MeetingInstrumentedTest.java
@@ -3,22 +3,14 @@ package com.example.mareu;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
-import com.example.mareu.MainActivity;
-import com.example.mareu.MyMeetingAdapter;
-import com.example.mareu.R;
-import com.example.mareu.di.DI;
 import com.example.mareu.model.Meeting;
-import com.example.mareu.service.DummyMeetingApiService;
-import com.example.mareu.service.DummyMeetingApiServiceGenerator;
-import com.example.mareu.service.MeetingApiService;
+
 import com.example.mareu.utils.DeleteMeetingAction;
 import com.example.mareu.utils.RecyclerViewMatcher;
 
@@ -29,23 +21,15 @@ import org.junit.runner.RunWith;
 
 import java.util.List;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.AllOf.allOf;
 import static com.example.mareu.utils.RecyclerViewItemCountAssertion.withItemCount;
 
@@ -57,16 +41,8 @@ import static com.example.mareu.utils.RecyclerViewItemCountAssertion.withItemCou
 @RunWith(AndroidJUnit4.class)
 public class MeetingInstrumentedTest {
 
-    private List<Meeting> test;
-
     //number of meetings at when apps is launched
     private static int ITEM_COUNT = 3;
-    private MyMeetingAdapter mAdapter;
-
-    public void setup(){
-        //MyMeetingAdapter.mMeetingList.size();
-        //currentMeetingListSize = test.size();
-    }
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
@@ -130,32 +106,77 @@ public class MeetingInstrumentedTest {
     }
 
     @Test
-    public void orderingByPlaceShouldWorks(){
+    public void orderingByDateShouldWorks(){
+        onView(withId(R.id.item_menu))
+                .perform(click());
+        onView(withText("Du plus récent au plus ancien"))
+                .perform(click());
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(0, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion B - 16:00 - Mario"))); //date 31/03/2021
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(1, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion C - 19:00 - Luigi"))); //date 18/11/2020
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(2, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion A - 14:00 - Peach"))); // date 01/01/2019
         onView(withId(R.id.item_menu))
                 .perform(click());
         onView(withText("Du plus ancien au plus récent"))
                 .perform(click());
         onView(new RecyclerViewMatcher(R.id.recyclerViewList)
                 .atPositionOnView(0, R.id.meeting_information_TV))
-                .check(matches(withText("Réunion C - 19:50 - Luigi")));
+                .check(matches(withText("Réunion A - 14:00 - Peach"))); // date 01/01/2019
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(1, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion C - 19:00 - Luigi"))); // date 18/11/2020
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(2, R.id.meeting_information_TV)) // date 31/03/2021
+                .check(matches(withText("Réunion B - 16:00 - Mario")));
+    }
+
+    @Test
+    public void orderingByPlaceShouldWorks(){
         onView(withId(R.id.item_menu))
                 .perform(click());
-        onView(withText("Du plus récent au plus ancien"))
+        onView(withText("Lieu de réunion: A -> Z"))
                 .perform(click());
         onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(0, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion C - 19:00 - Luigi")));
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(1, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion B - 16:00 - Mario")));
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
                 .atPositionOnView(2, R.id.meeting_information_TV))
-                .check(matches(withText("Réunion C - 19:50 - Luigi")));
-    }
+                .check(matches(withText("Réunion A - 14:00 - Peach")));
+        onView(withId(R.id.item_menu))
+                .perform(click());
+        onView(withText("Lieu de réunion: Z -> A"))
+                .perform(click());
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(0, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion A - 14:00 - Peach")));
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(1, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion B - 16:00 - Mario")));
+        onView(new RecyclerViewMatcher(R.id.recyclerViewList)
+                .atPositionOnView(2, R.id.meeting_information_TV))
+                .check(matches(withText("Réunion C - 19:00 - Luigi")));
 
+
+    }
+/*
     @Test
     public void shouldRemoveMeeting() {
+
+    }
+*/
+    @Test
+    public void shouldRemoveAndAddMeeting(){
         onView(withId(R.id.recyclerViewList))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, new DeleteMeetingAction()));
-        onView(allOf(withId(R.id.recyclerViewList), isDisplayed())).check(withItemCount(ITEM_COUNT - 1));
-    }
-
-    @Test
-    public void shouldAddMeeting(){
+        onView(withId(R.id.recyclerViewList)).check(withItemCount(ITEM_COUNT - 1));
         onView(withId(R.id.fab_add_reunion))
                 .perform(click());
         onView(withId(R.id.meeting_subject_ET))
@@ -172,27 +193,7 @@ public class MeetingInstrumentedTest {
         onView(withText("OK")).perform(click());
         onView(withText("CONFIRMER"))
                 .perform(click());
-        onView(allOf(withId(R.id.recyclerViewList), isDisplayed())).check(withItemCount(ITEM_COUNT));
+        onView(withId(R.id.recyclerViewList)).check(withItemCount(ITEM_COUNT));
     }
-
-    @Test
-    public void orderingByDateShouldWorks(){
-        onView(withId(R.id.item_menu))
-                .perform(click());
-        onView(withText("Du plus ancien au plus récent"))
-                .perform(click());
-        onView(withId(R.id.recyclerViewList))
-                .perform(RecyclerViewActions.scrollToPosition(0));
-        onView(withId(R.id.recyclerViewList))
-                .check(matches(withText("")));
-        /*
-       onView(withId(R.id.item_menu))
-                .perform(click());
-        onView(withText("Du plus récent au plus ancien"))
-                .perform(click());
-        */
-
-    }
-
 
 }
--- app/src/main/java/com/example/mareu/service/DummyMeetingApiServiceGenerator.java
+++ app/src/main/java/com/example/mareu/service/DummyMeetingApiServiceGenerator.java
@@ -14,8 +14,8 @@ public abstract class DummyMeetingApiServiceGenerator {
 
     public static List <Meeting> MEETING_LIST = Arrays.asList(
             new Meeting("Réunion A","14:00","Peach","test@gmail.com, test2@gmail.com, test3@gmail.com","01/01/2019"),
-            new Meeting("Réunion B","16:00","Mario","test@gmail.com, test2@gmail.com, test3@gmail.com","25/02/2020"),
-            new Meeting("Réunion C","19:50","Luigi","test@gmail.com, test2@gmail.com, test3@gmail.com","30/12/2021")
+            new Meeting("Réunion B","16:00","Mario","test@gmail.com, test2@gmail.com, test3@gmail.com","31/03/2021"),
+            new Meeting("Réunion C","19:00","Luigi","test@gmail.com, test2@gmail.com, test3@gmail.com","18/11/2020")
     );
 
 }
